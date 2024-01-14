ea42df5a57d7b676a26de0e789a642d58c6bd793
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-14 17:37:37 -0300

    feat(models): define model collumns

---
+++ users/user.js
@@ -0,0 +1,30 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../sequelize/sequelize.js";
+
+const User = sequelize.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descricao: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+export default User
