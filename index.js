5eda32e9a671d220b46734a73ff90762b355a34f
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-14 17:09:35 -0300

    feat(index): server listening in port 3000

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import app from './config/config.js'
 import express from 'express'
 import { Sequelize } from 'sequelize'
 import { createConnection } from 'mysql2'
-import connection from './connections/database.js'
+import connection from './models/database.js'
+import Sequelize from 'sequelize'
+import sequelize from './sequelize/sequelize.js'
+import './routes.js'
+
+sequelize.sync(()=>{ console.log('Mysql database listenning in port 3306') })
+app.listen(3000, ()=>{
+    console.log('Server listening in port 3000')
+})
