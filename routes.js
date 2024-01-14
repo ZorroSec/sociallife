458c4f52970c7e6fcabf609c800d81a20df58a00
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-14 19:31:49 -0300

    feat(fetch): create a new fetch in initial route(/)

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import app from "./config/config.js";
 import connection from "./models/database.js";
 import Post from './post/post.js';
+import fetchIp from './ip/fetchIp.js';
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -10,7 +11,7 @@ app.use(express.static('css'))
 app.get('/', (rq, res)=>{
     fetch("https://api.ipify.org/?format=json").then(response=>{
         response.json().then(data=>{
-            res.json(data)
+            return data
         })
     })
     // res.json({
