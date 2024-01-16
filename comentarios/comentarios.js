36ed945ebe4284b34fe908436342ce01c0d7de7f
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-16 18:52:19 -0300

    feat(model): create a new model Comentario

---
+++ comentarios/comentarios.js
@@ -0,0 +1,27 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const sequelize = require('../sequelize/sequelize.js')
+
+const Comentario = sequelize.define('comentarios', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    idpost: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    comentario: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dataComentario: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+})
